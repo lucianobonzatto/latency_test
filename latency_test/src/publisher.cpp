b5c28b4b97766af7acafbd737733d46f702c0afa
luciano <luciano.bj23@hotmail.com>
2024-09-07 22:14:11 -0300

    create publisher list

--- latency_test/src/publisher.cpp
+++ latency_test/src/publisher.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <memory>
 #include <fstream>
+#include <vector>
 
 #include <rclcpp/rclcpp.hpp>
 #include "latency_test_msgs/msg/data.hpp"
@@ -18,7 +19,7 @@ public:
         message.sequence_number = 0;
         message.header.frame_id = "base_frame";
 
-        // Criar o serviço, mas não o publicador ainda
+        // Criar o serviço, mas não os publishers ainda
         service_ = this->create_service<latency_test_msgs::srv::PublishRequest>("start_publisher", 
             [this](const std::shared_ptr<latency_test_msgs::srv::PublishRequest::Request> request,
             std::shared_ptr<latency_test_msgs::srv::PublishRequest::Response> response)
@@ -31,14 +32,20 @@ private:
             std::shared_ptr<latency_test_msgs::srv::PublishRequest::Response> response)
     {
         test_request = *request;
+        int num_publishers_ = 4;
         RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
                     "request: \n\tdata_size: %ld publish_interval: %ld",
                     test_request.size,
                     test_request.publish_interval);
 
-        // Criar o publicador somente quando o serviço é chamado
-        publisher_ = this->create_publisher<latency_test_msgs::msg::Data>(
-            "chatter", 10);
+        // Criar o número de publishers definido por num_publishers_
+        publishers_.clear(); // Limpar qualquer publisher existente
+        for (int i = 0; i < num_publishers_; ++i)
+        {
+            auto publisher = this->create_publisher<latency_test_msgs::msg::Data>(
+                "chatter", 10);
+            publishers_.push_back(publisher);
+        }
 
         // Preparar a mensagem
         message.sequence_number = 0;
@@ -49,13 +56,19 @@ private:
         // Definir um temporizador para publicar mensagens
         timer_ = this->create_wall_timer(milliseconds(test_request.publish_interval), 
             [this](){
-            message.header.stamp = this->now();
-            publisher_->publish(message); 
+
+            int index = 0;
+            for (auto& publisher : publishers_)
+            {
+                message.header.stamp = this->now();
+                publisher->publish(message);
+                index++;
+            }
             message.sequence_number++;
             if (message.sequence_number == OUT_SIZE)
             {
                 timer_->cancel();
-                publisher_.reset();
+                publishers_.clear(); // "Eliminar" os publishers
                 message.sequence_number = 0;
             }
         });
@@ -63,7 +76,7 @@ private:
         response->response = 1;
     }
 
-    rclcpp::Publisher<latency_test_msgs::msg::Data>::SharedPtr publisher_;
+    std::vector<rclcpp::Publisher<latency_test_msgs::msg::Data>::SharedPtr> publishers_;
     rclcpp::Service<latency_test_msgs::srv::PublishRequest>::SharedPtr service_;
     rclcpp::TimerBase::SharedPtr timer_;
     latency_test_msgs::msg::Data message;
@@ -74,6 +87,7 @@ int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<PublishTestNode>();
+
     rclcpp::spin(node);
 
     rclcpp::shutdown();
